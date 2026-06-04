444b4b662794b2612f60bef930ba161ba49877d8
yubin05 <vundle34@naver.com>
2026-06-05 00:20:42 +0900

    배포 자동화 빌드 파트 최적화

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -32,7 +32,7 @@ resource "aws_ecr_repository" "review" {
 
 resource "aws_ecr_repository" "support" {
   name                 = "support-service"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   force_delete         = true
   image_scanning_configuration { scan_on_push = true }
   tags = { Name = "support-service" }
